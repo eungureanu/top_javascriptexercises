cec83ded52be4d321337169a3b8e6561e8c8aa52
eungureanu <64928001+eungureanu@users.noreply.github.com>
2023-02-26 21:05:04 +0200

    Upload TOP JS exercise07

---
+++ 07_tempConversion/tempConversion.js
@@ -0,0 +1,13 @@
+//the official solution uses Math.round
+const convertToCelsius = function(tempF) {
+  return +((tempF-32)*5/9).toFixed(1);
+};
+
+const convertToFahrenheit = function(tempC) {
+  return +(tempC*9/5+32).toFixed(1);
+};
+// Do not edit below this line
+module.exports = {
+  convertToCelsius,
+  convertToFahrenheit
+};
